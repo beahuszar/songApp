7221a1577ddc0d78d561f88a6fd886b21706eb56
Huszar Beata <beata.huszar@adevinta.com>
2019-10-23 17:33:42 +0200

    Building list with Redux data

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Songlist extends Component {
+  renderlist() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
     return (
-      <div>Songs</div>
+      <div className="ui divided list">{this.renderlist()}</div>
     );
   };
 };
